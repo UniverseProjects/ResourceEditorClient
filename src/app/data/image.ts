3225804d8bfba37c22c180eb6a771cae59f245e5
Paul Korepanov <paul.kore@gmail.com>
2017-08-16 19:30:39 -0400

    Make images tab load several images from the Google Cloud server (temporary)

---
+++ src/app/data/image.ts
@@ -0,0 +1,6 @@
+export class Image {
+    url: string = 'foo';
+    constructor(url: string) {
+        this.url = url;
+    }
+}
--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,7 +1,22 @@
 
 import {Injectable} from '@angular/core';
+import {Image} from '../data/image';
+
+const IMAGES: Image[] = [
+    new Image('https://storage.googleapis.com/up-resource-library/images/ships/NimbleShip1.png'),
+    new Image('https://storage.googleapis.com/up-resource-library/images/ships/NimbleShip2.png'),
+    new Image('https://storage.googleapis.com/up-resource-library/images/ships/NimbleShip3.png'),
+    new Image('https://storage.googleapis.com/up-resource-library/images/ships/Artist1GreenShip.png'),
+    new Image('https://storage.googleapis.com/up-resource-library/images/ships/Artist2Ship1.png'),
+    new Image('https://storage.googleapis.com/up-resource-library/images/ships/FastShip1.png'),
+    new Image('https://storage.googleapis.com/up-resource-library/images/ships/JunkerShip1.png'),
+    new Image('https://storage.googleapis.com/up-resource-library/images/ships/ViperMk2.png'),
+    new Image('https://storage.googleapis.com/up-resource-library/images/ships/WraithShip1.png'),
+];
 
 @Injectable()
 export class ImageService {
-
+    getImages(): Promise<Image[]> {
+        return Promise.resolve(IMAGES);
+    }
 }
