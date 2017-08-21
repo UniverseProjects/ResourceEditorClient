8801989b40521609ee5ce3ca3eebcbc00262e0dc
Paul Korepanov <paul.kore@gmail.com>
2017-08-21 18:56:47 -0400

    Remove project contents (this was built from QuickStart), to be replaced with Angular-CLI based project

--- src/app/components/dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'dashboard-comp',
-    template: `<h2>Dashboard</h2>`,
-})
-export class DashboardComponent {
-
-}
--- src/app/data/image.ts
+++
@@ -1,6 +0,0 @@
-export class Image {
-    url: string = 'foo';
-    constructor(url: string) {
-        this.url = url;
-    }
-}
