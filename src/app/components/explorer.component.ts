6c5ffdf55a4ff0ff09e101544b2ec287d04b33cc
Paul Korepanov <paul.kore@gmail.com>
2017-09-01 17:26:53 -0400

    Incorporate HTML and CSS into "explorer.component.ts" - the contents are too small to justify stand-alone files

--- src/app/components/explorer.component.ts
+++ src/app/components/explorer.component.ts
@@ -2,8 +2,19 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-explorer',
-  templateUrl: './explorer.component.html',
-  styleUrls: ['./explorer.component.css'],
+  template: `
+    <app-directory-tree></app-directory-tree>
+    <div class="image-container">
+      <app-images></app-images>
+    </div>
+  `,
+  styles: [`
+    .image-container {
+      width: 500px;
+      display: inline-block;
+      vertical-align: top;
+    }
+  `],
 })
 export class ExplorerComponent {
 
