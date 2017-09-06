7dce8a8c16748eff3282f7bff6633d966105ccf0
Paul Korepanov <paul.kore@gmail.com>
2017-09-05 22:04:24 -0400

    Bundle HTML and CSS into TS file (they are too small)

--- src/app/components/thumbnails.component.ts
+++ src/app/components/thumbnails.component.ts
@@ -2,6 +2,33 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-thumbnails',
+  styles: [`
+    .image-container {
+      width: 100px;
+      height: 100px;
+      border: solid #888 1px;
+      margin: 0 5px 5px 0;
+      padding: 2px;
+      display: inline-block;
+      vertical-align: top;
+      position: relative;
+    }
+    .image-container:hover {
+      background-color: #ade4ff;
+    }
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      object-fit: contain;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+    
+      -ms-transform: translate(-50%, -50%); /* IE 9 */
+      -webkit-transform: translate(-50%, -50%); /* Safari */
+      transform: translate(-50%, -50%);
+    }
+  `],
   template: `<div class="images">
     <div *ngFor="let imageUrl of imageUrls; let i = index"
          (click)="onSelect(i)"
@@ -9,7 +36,6 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
       <img src="{{imageUrl}}"/>
     </div>
   </div>`,
-  styleUrls: [`./thumbnails.component.css`],
 })
 export class ThumbnailsComponent {
   @Input() imageUrls: string[] = [];
