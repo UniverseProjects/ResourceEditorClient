631f572a7271903edfdf770e18e48b57332363e1
Paul Korepanov <paul.kore@gmail.com>
2017-09-15 17:18:38 -0400

    minor

--- src/app/services/explorer.service.ts
+++ src/app/services/explorer.service.ts
@@ -1,7 +1,12 @@
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
 
-export enum ContentType { IMAGES, SPRITES, ANIMATED_SPRITES, DIRECTORIES }
+export enum ContentType {
+  IMAGES,
+  SPRITES,
+  ANIMATED_SPRITES,
+  DIRECTORIES
+}
 
 @Injectable()
 export class ExplorerService {
