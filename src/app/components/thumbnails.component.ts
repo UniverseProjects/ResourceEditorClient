0da40f6202e34a03c67e9e8e65205b6cc6fbae83
Paul Korepanov <paul.kore@gmail.com>
2017-10-03 18:02:24 -0400

    rename container class

--- src/app/components/thumbnails.component.ts
+++ src/app/components/thumbnails.component.ts
@@ -29,7 +29,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
       transform: translate(-50%, -50%);
     }
   `],
-  template: `<div class="images">
+  template: `<div class="thumbnails-container">
     <div *ngFor="let imageUrl of imageUrls; let i = index"
          (click)="onSelect(i)"
          class="image-container" [class.selected]="false">
