f0cb1cd6cb28d1c73f3bc7fb3ec57212839c3333
Paul Korepanov <paul.kore@gmail.com>
2017-10-10 15:09:20 -0400

    Introduce common functions

---
+++ src/app/common/common.ts
@@ -0,0 +1,13 @@
+/** Commonly-used functions */
+// TODO: see if it's possible to make these global functions (attempted once, however wasn't able to get it to work at runtime)
+export class C {
+
+  static defined(value: any): boolean {
+    return value !== null && value !== undefined;
+  }
+
+  static xor(a: any, b: any): boolean {
+    return (a && !b) || (!a && b);
+  }
+
+}
