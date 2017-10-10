9611c8ec9f4b7319b504c2458d1eb95e1f64a2a5
Paul Korepanov <paul.kore@gmail.com>
2017-10-10 16:23:13 -0400

    Replace thumbnails implementation with one that uses the ImageFrameComponent for each thumbnail

--- src/app/components/thumbnails.component.ts
+++ src/app/components/thumbnails.component.ts
@@ -3,36 +3,26 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 @Component({
   selector: 'app-thumbnails',
   styles: [`
-    .image-container {
-      width: 100px;
-      height: 100px;
-      border: solid #888 1px;
+    .thumbnail-container {
       margin: 0 5px 5px 0;
-      padding: 2px;
       display: inline-block;
       vertical-align: top;
-      position: relative;
     }
-    .image-container:hover {
+    .thumbnail-container:hover {
       background-color: #ade4ff;
-    }
-    img {
-      max-width: 100%;
-      max-height: 100%;
-      object-fit: contain;
-      position: absolute;
-      left: 50%;
-      top: 50%;
-    
-      -ms-transform: translate(-50%, -50%); /* IE 9 */
-      -webkit-transform: translate(-50%, -50%); /* Safari */
-      transform: translate(-50%, -50%);
     }
   `],
   template: `
     <div class="thumbnails-container">
-      <div class="image-container" *ngFor="let imageUrl of imageUrls; let i = index" (click)="onSelect(i)">
-        <img src="{{imageUrl}}"/>
+      <div class="thumbnail-container" *ngFor="let imageUrl of imageUrls; let i = index" (click)="onSelect(i)">
+        <app-image-frame 
+          [properties]="{
+            imageUrl: imageUrl, 
+            width: 100, 
+            height: 100, 
+            fitFrame: true, 
+            imageBorder:false
+          }"></app-image-frame>
       </div>
     </div>
   `,
