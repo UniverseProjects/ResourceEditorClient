2b1bd4de0681bbc1a56849d32f9f5d2006fb1dd6
Paul Korepanov <paul.kore@gmail.com>
2017-10-27 09:16:28 -0400

    minor

--- src/app/common/api.helper.ts
+++ src/app/common/api.helper.ts
@@ -2,7 +2,7 @@ import {PathUtil} from './path.util';
 
 export class ApiHelper {
 
-  public static readonly BASE_URL = 'https://www.universeprojects.com/api/v1/';
+  static readonly BASE_URL = 'https://www.universeprojects.com/api/v1/';
 
   static path(treePath: string): string {
     if (!PathUtil.isValid(treePath)) {
